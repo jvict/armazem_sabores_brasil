1d97b6aedf4dae934479bb70567ad72560fa81db
João Victor Tiburcio <jovitiburcio@rd.com.br>
2023-06-01 17:26:23 -0300

    Create and edit form in page Budget

--- components/Form.tsx
+++ components/Form.tsx
@@ -4,23 +4,35 @@ import { useState } from "react";
 
 export const Form = () => {
     const [eventName, setEventName] = useState('');
+    const [getLocal, setLocal] = useState('');
+    const [getQtyPersons, setQtyPersons] = useState('');
     const [finishPrevisionDate, setFinishPrevisionDate] = useState('');
     const route = useRouter()
     return (
         <>
             <div className="container-form">
-                <div className="form-header">
+                <div className="form-body">
                     <p>Adiocionar um evento</p>
                     <input type="text" placeholder="Nome do evento"
                         value={eventName} onChange={e => setEventName(e.target.value)} />
 
                     <input type='text' placeholder="Data do evento" onFocus={(e) => { e.target.type = "date" }} onBlur={(e) => e.target.type = "text"}
                         value={finishPrevisionDate} onChange={e => setFinishPrevisionDate(e.target.value)} />
+                    
+                    <input type='text' placeholder="Local" 
+                        value={getLocal} onChange={e => setLocal(e.target.value)} />
+                    
+                    <input type='text' placeholder="Quantidade de Pessoas" 
+                        value={getQtyPersons} onChange={e => setQtyPersons(e.target.value)} />
+                        
 
                 </div>
 
-                <div className="form-footer">
+                <div className="button col-12">
                     <button onClick={() => route.back()}>Voltar</button>
+
+                    <button onClick={() => route.back()}>Criar</button>
+
                 </div>
             </div>
         </>
